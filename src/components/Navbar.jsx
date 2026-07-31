2437dc6d38d4b55d15098a61547fdb1b74d63b35
Muhammad Izzudin Zaki <dahlahgatau10@gmail.com>
2026-07-31 23:27:24 +0700

    feat: update site favicon and add logo to navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import PillNav from './PillNav';
 import { NAV_ITEMS } from '../constants';
+import logoImg from '../assets/logo.png';
+
 const Navbar = () => {
     const location = useLocation();
 
     return (
         <PillNav
+            logo={logoImg}
+            logoAlt="Zaki Portfolio Logo"
             items={NAV_ITEMS}
             activeHref={location.pathname}
             baseColor="#ffffff" // Expanding hover circle (pure white)
